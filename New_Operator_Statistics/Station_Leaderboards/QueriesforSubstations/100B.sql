dc5c631517ba65e50ad0996c4d200e82afb28069
Ahmed Qazi <91359561+an-qazi@users.noreply.github.com>
2025-08-20 15:39:06 -0400

    Create 100B.sql

---
+++ New_Operator_Statistics/Station_Leaderboards/QueriesforSubstations/100B.sql
@@ -0,0 +1,21 @@
+Select Top 5
+	FULLNAME AS Operator, -- operator name
+	Round(Avg(Cast(VAL AS Float)), 2) as Mean_Cycle_Time --  average time to 2 decimal places
+
+From ABC.OPCYCLE_ALL
+
+Where
+	ASSET = '100B' 	-- only this station
+	and DT >= DATEADD(week,
+	datediff(week, '1900-01-07', getdate()), -- get # of weeks from known sunday
+	cast('1900-01-07' AS date)) -- start date, add weeks from this date
+	-- starts data at Sunday of current week
+	and DT <= getdate() 
+	and VAL between 3 and 100 -- plausible times only
+	and FULLNAME <> 'N/A' -- exclude no operator entries
+
+Group By FULLNAME
+
+Having COUNT(*) >= 200 -- must have at least 200 cycles
+
+Order By Mean_Cycle_Time ASC;
