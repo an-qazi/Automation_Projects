07085e6a64c1df60cf5559b75e21cc28850f0a61
Ahmed Qazi <91359561+an-qazi@users.noreply.github.com>
2025-08-20 15:27:02 -0400

    Create 520.sql

---
+++ New_Operator_Statistics/Station_Leaderboards/QueriesforLines/520.sql
@@ -0,0 +1,21 @@
+Select Top 8
+	FULLNAME AS Operator, -- operator name
+	Round(Avg(Cast(VAL AS Float)), 2) as Mean_Cycle_Time -- their average time to 2 decimal places
+
+From ABC.OPCYCLE_ALL
+
+Where
+	Left(ASSET, 3) = '520' 	-- only this family
+	and DT >= DATEADD(week, -- use weeks
+	datediff(week, '1900-01-07', getdate()), -- get # of weeks from known sunday
+	cast('1900-01-07' AS date)) -- start date, add weeks from this date
+	-- starts data at Sunday of current week
+	and DT <= getdate() 
+	and VAL between 3 and 100 -- plausible times only
+	and FULLNAME <> 'N/A' -- exclude no operator entries
+
+Group By FULLNAME
+
+Having COUNT(*) >= 200 -- must have at least 200 cycles
+
+Order By Mean_Cycle_Time ASC;
